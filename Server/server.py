9d0ddaf39515b95d8de7c7d1eaf5c58a3b57526b
Elena Marzi Tornblad <elena@manele.se>
2022-07-06 11:14:43 +0200

    added a database and post

--- Server/server.py
+++ Server/server.py
@@ -1,5 +1,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
+from urllib.parse import parse_qs
+
 
 hostName = "localhost"
 serverPort = 8080
@@ -17,11 +19,7 @@ def get_content_type(filename):
 
 
 class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        path = "./Client" + self.path
-        if path.endswith("/"):
-            path = path + "index.html"
-
+    def http_response(self, path):
         self.send_response(200)
         self.send_header("Content-type", get_content_type(path))
         self.end_headers()
@@ -30,6 +28,32 @@ class MyServer(BaseHTTPRequestHandler):
             lines = f.read()
 
         self.wfile.write(bytes(lines, encoding='utf8'))
+
+    def do_GET(self):
+        path = "./Client" + self.path
+        if path.endswith("/"):
+            path = path + "index.html"
+        self.http_response(path)
+
+    def do_POST(self):
+        length = int(self.headers['content-length'])
+        postvars = parse_qs(self.rfile.read(length),
+                            keep_blank_values=1)
+
+        username = postvars[bytes('username', encoding='utf8')]
+        password = postvars[bytes('password', encoding='utf8')]
+        password = password[0].decode("utf-8")
+        username = username[0].decode("utf-8")
+        username_and_password = username + " " + password
+
+        f = open("./Database/db.txt", "r")
+        for line in f:
+            if line.strip() == username_and_password:
+
+                self.http_response("./Client/MFA.html")
+                # communication with Arduino
+                return
+        self.http_response("./Client/index.html")
 
 
 if __name__ == "__main__":
