ac13ea32663ba2c1ce2146417490ec1d727b06f5
Elena Marzi Tornblad <elena@manele.se>
2022-07-06 18:15:50 +0200

    frontend done

---
+++ Client/script.js
@@ -0,0 +1,44 @@
+const form = document.getElementById("form");
+const timer = document.getElementById("timer");
+const mfa = document.getElementById("code-mfa");
+const new_code = document.getElementById("new-code-button");
+const verify = document.getElementById("verify-button");
+const title = document.getElementById("title");
+const info = document.getElementById("extra-info");
+
+function get_new_code(e) {
+  e.preventDefault();
+  initiate_counter(3);
+}
+
+
+function initiate_counter(time) {
+  timer.textContent = time;
+  title.textContent = "Code sending to Arduino in";
+  timer.style.display = "";
+  info.style.display = "";
+  mfa.style.display = "none";
+  verify.style.display = "none";
+  new_code.style.display = "none";
+  countdown(time);
+}
+
+function countdown(time) {
+  timer.textContent = time;
+  if (time == 0) {
+    title.textContent = "Code sent";
+    timer.style.display = "none";
+    info.style.display = "none";
+    mfa.style.display = "";
+    new_code.style.display = "";
+    verify.style.display = "";
+    return;
+  }
+  window.setTimeout(() => {
+    countdown(time - 1);
+  }, 1000);
+
+}
+
+initiate_counter(3);
+new_code.addEventListener('click', get_new_code);
