b405d7de65f4ddcfa689825a21112d83289d1fbb
Elena Marzi Tornblad <elena@manele.se>
2022-07-07 10:17:25 +0200

    binary light

---
+++ MFA/MFA.ino
@@ -0,0 +1,27 @@
+// the setup function runs once when you press reset or power the board
+void setup() {
+  
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(4, OUTPUT);  
+
+  blink_binary(14); 
+  
+}
+
+void blink_binary(int value){
+
+  for(int i = 0 ; i<4; i++){
+    if(value & (1 << i)) 
+        digitalWrite(7-i, HIGH); 
+    else 
+         digitalWrite(7-i, LOW); 
+  }
+ 
+}
+// the loop function runs over and over again forever
+void loop() {
+
+ 
+}
